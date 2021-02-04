e387604339463d693faa70f0d2da31cd069f0e08
Zejun Xiong <69989842+MichaelThePanda@users.noreply.github.com>
2021-02-03 21:33:58 -0800

    Everything finished

--- GridWorldPart4/src/info/gridworld/critters/BlusterCritter.java
+++ GridWorldPart4/src/info/gridworld/critters/BlusterCritter.java
@@ -12,25 +12,23 @@ public class BlusterCritter extends Critter{
 	private int c = 8;
 //	private int x = getLocation().getRow();
 //	private int y = getLocation().getCol();
-	private static final double COLOR_FACTOR = 0.5;
+	private static final double COLOR_FACTOR = 0.3;
 
 	public ArrayList<Actor> getActors() {
 	 	 int x = getLocation().getCol();
 		 int y = getLocation().getRow();
-		 System.out.println("Location: x = "+x+", y = "+y);
 		 ArrayList<Actor> actors =  getGrid().getNeighbors(getLocation());
 		 
-		 Location one = new Location(x-1, y-1);
-		 Location two = new Location(x+1, y-1);
-		 Location three = new Location(x+1, y+1);
-		 Location four = new Location(x-1, y+1);
+		 Location one = new Location(y-1, x-1);
+		 Location two = new Location(y-1, x+1);
+		 Location three = new Location(y+1, x+1);
+		 Location four = new Location(y+1, x-1);
 		 
 		 ArrayList <Location> subCritters = new ArrayList<Location>();
 		 subCritters.add(one);
 		 subCritters.add(two);
 		 subCritters.add(three);
 		 subCritters.add(four);
-		 System.out.println("subCritters list: " + subCritters);
 		 for(int a=0; a<4; a++) {
 			 if(getGrid().isValid(subCritters.get(a).getAdjacentLocation(Location.WEST+a*90)) && getGrid().get(subCritters.get(a).getAdjacentLocation(Location.WEST+a*90)) != null) {
 				 actors.add(getGrid().get(subCritters.get(a).getAdjacentLocation(Location.WEST+a*90)));
@@ -42,57 +40,46 @@ public class BlusterCritter extends Critter{
 		    	 actors.add(getGrid().get(subCritters.get(a).getAdjacentLocation(Location.NORTHEAST+a*90)));
 		     }
 		 }
-		 System.out.println("Actor List: "+actors);
 		 return actors;
 	}
-	/*
-	public ArrayList<Actor> getActors() {
-		
-		for(int a=x-2; a<=x+2; a++) {
-			
-		}
-		
-		
-		return null;
-	}*/
 
 	public void processActors(ArrayList<Actor> actors) {
 		
 			int n = actors.size();
-			System.out.println("n = "+n);
         	Color c = getColor();
         	int red = (int) (c.getRed());
         	int green = (int) (c.getGreen());
         	int blue = (int) (c.getBlue());
-	        if (n<8) {
-	        	if(red<255) {
-		        	red *= (1+COLOR_FACTOR);
+	        if (n<2) {
+	        	blue *= (1+COLOR_FACTOR); 
+	        	green *= (1+COLOR_FACTOR);
+	        	red *= (1+COLOR_FACTOR);
+	        	if(red>255) {
+	        		red = 255;
 	        	}
-	        	if(green<255) {
-		        	green *= (1+COLOR_FACTOR);
+	        	if(green>255) {
+	        		green = 255;
 	        	}
-	        	if(blue<255) {
-		        	blue *= (1+COLOR_FACTOR); 
+	        	if(blue>255) {
+	        		blue = 255;
 	        	}
 	        	setColor(new Color(red, green, blue));        	
 	            return;
-	        }else if(n>8) {
-	        	if(red>1) {
-		        	red *= (1-COLOR_FACTOR);
+	        }else if(n>2) {
+	        	blue *= (1-COLOR_FACTOR); 
+	        	green *= (1-COLOR_FACTOR);
+	        	red *= (1-COLOR_FACTOR);
+	        	if(red<1) {
+	        		red = 1;
 	        	}
-	        	if(green>1) {
-		        	green *= (1-COLOR_FACTOR);
+	        	if(green<1) {
+	        		green = 1;
 	        	}
-	        	if(blue>1) {
-		        	blue *= (1-COLOR_FACTOR); 
+	        	if(blue<1) {
+	        		blue = 1;
 	        	}
 	        	setColor(new Color(red, green, blue));        	
 	            return;
 	        }
-	        
-	        int r = (int) (Math.random() * n);
-
-	        Actor other = actors.get(r);
-	        setColor(other.getColor());
 	}
 }
--- GridWorldPart4/src/info/gridworld/critters/HoarderCritter.java
+++ GridWorldPart4/src/info/gridworld/critters/HoarderCritter.java
@@ -1,7 +1,116 @@
 package info.gridworld.critters;
 
+import java.util.ArrayList;
+
+import info.gridworld.actor.Actor;
 import info.gridworld.actor.Critter;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.Location;
 
 public class HoarderCritter extends Critter{
+		
+	private Location home;
+	private Actor pray;
+	private boolean moveAway = false;
+	private int count = 0;
+	
+	public HoarderCritter() {
+		home = new Location(0,0);
+	}
+	
+	public HoarderCritter(Location a) {
+		home = a;
+	}
 
+	public void processActors(ArrayList<Actor> actors) {
+		/*
+        for(int a=0; a<actors.size();a++) {
+        	if(actors.get(a) instanceof Actor) {
+        		actors.remove(a);
+        		a--;
+        	}
+        }*/
+		if (pray == null) {
+	        int n = actors.size();
+	        if (n != 0) {
+	            int r = (int) (Math.random() * n);
+	            pray = actors.get(r);
+	            for (Actor b : actors) {
+	                if (b==pray)
+	                    b.removeSelfFromGrid();
+	            }
+	        }
+		}
+    }
+	
+	public ArrayList<Location> getMoveLocations() {
+		ArrayList<Location> loc = new ArrayList<Location>();
+		Location tempLoc;
+			if(moveAway == false && pray != null) {
+				if(home.getRow()>getLocation().getRow()) {
+					tempLoc = new Location(getLocation().getRow()+1, getLocation().getCol());
+					if((getGrid().get(tempLoc)==null && getGrid().isValid(tempLoc)) || tempLoc == home)
+						loc.add(0, tempLoc);
+				}else if(home.getRow()<getLocation().getRow()) {
+					tempLoc = new Location(getLocation().getRow()-1, getLocation().getCol());
+					if((getGrid().get(tempLoc)==null && getGrid().isValid(tempLoc)) || tempLoc == home)
+						loc.add(0, tempLoc);
+				}else {
+					if(home.getCol()>getLocation().getCol()) {
+						tempLoc = new Location(getLocation().getRow(), getLocation().getCol()+1);
+						if((getGrid().get(tempLoc)==null && getGrid().isValid(tempLoc)) || tempLoc == home)
+							loc.add(0, tempLoc);
+					}else if(home.getCol()<getLocation().getCol()) {
+						tempLoc = new Location(getLocation().getRow()+1, getLocation().getCol()-1);
+						if((getGrid().get(tempLoc)==null && getGrid().isValid(tempLoc)) || tempLoc == home)
+							loc.add(0, tempLoc);
+					}else {
+						loc.add(0, new Location(getLocation().getRow()+1, getLocation().getCol()));
+						moveAway = true;
+					}
+				}
+			}else if(moveAway == true && pray == null){
+				if(home.getRow()<=getLocation().getRow()) {
+					tempLoc = new Location(getLocation().getRow()+1, getLocation().getCol());
+					if(getGrid().get(tempLoc)==null && getGrid().isValid(tempLoc))
+						loc.add(0, tempLoc);
+				}else if(home.getRow()>getLocation().getRow()) {
+					tempLoc = new Location(getLocation().getRow()-1, getLocation().getCol());
+					if(getGrid().get(tempLoc)==null && getGrid().isValid(tempLoc))
+						loc.add(0, tempLoc);
+				}else {
+					if(home.getCol()<=getLocation().getCol()) {
+						tempLoc = new Location(getLocation().getRow(), getLocation().getCol()+1);
+						if(getGrid().get(tempLoc)==null && getGrid().isValid(tempLoc))
+							loc.add(0, tempLoc);
+					}else if(home.getCol()>getLocation().getCol()) {
+						tempLoc = new Location(getLocation().getRow()+1, getLocation().getCol()-1);
+						if(getGrid().get(tempLoc)==null && getGrid().isValid(tempLoc))
+							loc.add(0, tempLoc);
+					}
+				}
+			}else {
+			loc = super.getMoveLocations();
+		}
+        return loc;
+    }
+    
+	public void makeMove(Location loc)
+    {
+		if(moveAway == true && pray != null) {
+            moveTo(loc);
+    		Actor newActor = pray;
+    		newActor.putSelfInGrid(getGrid(), home);
+    		pray = null;
+    	} else {
+    		moveTo(loc);
+        }
+        if(moveAway == true) {
+        	count++;
+        }
+        if(count > 2) {
+        	count = 0;
+        	moveAway = false;
+        }
+    }
 }
